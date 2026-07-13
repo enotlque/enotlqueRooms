2f4b9fa1496aedd88e0f23871863fad6c4730471
enotique <sasha.gryaznov.2018@gmail.com>
2026-07-13 16:16:14 +0400

    Аналогичные косметические изменения /duel

--- commands_economy.py
+++ commands_economy.py
@@ -2099,7 +2099,7 @@ class DiceButton(Button):
             challenger_roll2 = random.randint(1, 20)
             challenger_total = challenger_roll1 + challenger_roll2
 
-            result_embed = discord.Embed(title="Результаты игры в кости", color=int("6e6e6e", 16))
+            result_embed = discord.Embed(color=int("6e6e6e", 16))
             result_embed.set_author(name=f"Брошен вызов в кости - {self.author_name}",
                                      icon_url=interaction.guild.get_member(self.author_id).avatar.url)
 
@@ -2149,8 +2149,8 @@ class DiceButton(Button):
 
             result_note = discord.Embed(color=int("6e6e6e", 16))
             result_note.description = (f"<:winner:1299059106060959766> Победитель: **{winner_name}**\n"
-                                        f"<a:coinonrole:1298391257042784266> Чистый выигрыш: **{net_win}**\n"
-                                        f"-# Комиссия: {rake}")
+                                        f"<a:coinonrole:1298391257042784266> Чистый выигрыш: **{net_win}**")
+            result_note.set_footer(text=f"комиссия: {rake}")
             result_note.set_image(url="https://i.postimg.cc/jdv5cp6v/1111-1.png")
 
             view = View()
