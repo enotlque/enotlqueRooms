5017513fe7d539b2bca131429e5cb1c71c251386
enotique <sasha.gryaznov.2018@gmail.com>
2026-07-19 19:08:31 +0400

    ыхыхыхы

--- commands_profile.py
+++ commands_profile.py
@@ -53,9 +53,9 @@ LEFT_CENTER_X = 357
 LEFT_MAX_WIDTH = 400
 LEFT_VALUE_FONT_SIZE = 26
 LEFT_VALUES_Y = {
-    "role": 325,
-    "room": 525,
-    "marriage": 725,
+    "role": 285,
+    "room": 485,
+    "marriage": 685,
 }
 
 # Правая колонка (Баланс / В войсе / Сообщения / Место в топе).
